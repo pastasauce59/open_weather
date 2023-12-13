747fc75c982fa183f5245d00e015624cd020e76d
Michael Primavera <pastasauce59@gmail.com>
2023-12-12 22:37:49 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#From Open Weather API
+
+
+import requests
+
+response = requests.get(f'https://api.openweathermap.org/data/2.5/forecast?lat={my_lat}&lon={my_long}&appid={api_key}')
+status = response.status_code
+data = response.json()
+print(f"Status code: {status}")
+print(data)
