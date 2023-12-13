d3e30fd53d1f27b635c5c9fca94d80b0aaf5eaaa
Michael Primavera <pastasauce59@gmail.com>
2023-12-12 23:25:21 -0500

    Created parameters to use with get request to make it easier, searched through json response data to search for weather id codes to know if it would rain based on weather id numbers being below 700.

--- main.py
+++ main.py
@@ -3,8 +3,26 @@ from keys import *
 
 import requests
 
-response = requests.get(f'https://api.openweathermap.org/data/2.5/forecast?lat={my_lat}&lon={my_long}&appid={api_key}')
+weather_params = {
+    "lat": my_lat,
+    "lon" :my_long,
+    "appid": api_key,
+    "cnt": 4
+}
+
+response = requests.get('https://api.openweathermap.org/data/2.5/forecast', params=weather_params)
+response.raise_for_status()
 status = response.status_code
-data = response.json()
+weather_data = response.json()
 print(f"Status code: {status}")
-print(data)
+# print(weather_data["list"][0]["weather"][0]["id"])
+
+will_rain = False
+for hour_data in weather_data["list"]:
+    conditon_code = hour_data["weather"][0]["id"]
+    if int(conditon_code) < 700:
+        will_rain = True
+if will_rain:
+    print("Bring an umbrella.")
+else:
+    print("No need to worry about any rain or snow.")
